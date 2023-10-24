2e9c300a0138dde330e20ac6a56ecbac72d58f71
Ziang Chen <zeriars@gmail.com>
2023-10-24 04:22:54 -0400

    basic API controllers/routes implementation (404)

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,15 @@
 import express from 'express';
-import { config } from "dotenv"
-
+import { config } from "dotenv";
+import morgan from "morgan";
+import appRouter from './routes/index.js';
 config();
 const app = express();
 
 //middleware
 app.use(express.json());
 
+app.use(morgan("dev"));
+
+app.use("api/v1", appRouter);
 
 export default app;
