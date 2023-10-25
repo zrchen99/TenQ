3d49590b4741aea7e86cceda5b9161d86dbfff96
Ziang Chen <zeriars@gmail.com>
2023-10-25 17:42:20 -0400

    Instantiated basic front end components and pages

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createTheme, ThemeProvider } from "@mui/material";
+import { BrowserRouter } from "react-router-dom";
 import App from './App.tsx'
 import './index.css'
 
+const theme = createTheme({
+  
+  typography: {
+    fontFamily: "Roboto Slab, serif", 
+    allVariants:{color: "white"},
+  },
+})
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
