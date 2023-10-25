1653caba7987d2af2ce8fc7a97bf1e98e9914a45
Ziang Chen <zeriars@gmail.com>
2023-10-25 02:55:42 -0400

    Implemented User Signup/Login + validation

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,11 +1,12 @@
 import { NextFunction, Request, Response } from "express";
 import User from "../models/user.js";
+import { hash, compare } from "bcrypt";
 
 export const getAllUsers = async (
     req: Request,
     res: Response,
     next: NextFunction
-  ) => {
+    ) => {
     try {
       //get all users
       const users = await User.find();
@@ -15,3 +16,52 @@ export const getAllUsers = async (
       return res.status(200).json({ message: "ERROR FETCHING ALL USERS", cause: error.message });
     }
   };
+
+export const userSignup = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+    ) => {
+    try {
+        //user signup
+        const { name, email, password } = req.body;
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(401).json({ message: "USER ALREADY EXISTS" });
+        };
+        const hashedPassword = await hash(password, 10);
+        const user = new User({ name, email, password: hashedPassword });
+        await user.save();
+        //return user id
+        return res.status(201).json({ message: "USER SIGNUP", id: user._id.toString() });
+    } catch (error) {
+        console.log(error);
+        return res.status(200).json({ message: "ERROR SIGNUP", cause: error.message });
+    }
+};
+
+export const userLogin = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+    ) => {
+    try {
+        //user login
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        console.log(user);
+        if (!user) {
+            return res.status(401).json({ message: "USER DOES NOT EXIST" });
+        };
+        const isPasswordValid = await compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(403).json({ message: "INVALID CREDENTIALS" });
+        };
+
+        //return user id
+        return res.status(200).json({ message: "USER LOGIN", id: user._id.toString() });
+    } catch (error) {
+        console.log(error);
+        return res.status(200).json({ message: "ERROR SIGNUP", cause: error.message });
+    }
+};
--- backend/src/routes/user-routes.ts
+++ backend/src/routes/user-routes.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { getAllUsers } from "../controllers/user-controllers.js";
+import { getAllUsers, userLogin, userSignup } from "../controllers/user-controllers.js";
+import { loginValidator, signupValidator, validate } from "../utils/validators.js";
 
 const userRouter = Router();
 
 userRouter.get("/", getAllUsers);
+userRouter.post("/signup", validate(signupValidator), userSignup);
+userRouter.post("/login", validate(loginValidator), userLogin);
+
 
 export default userRouter;
